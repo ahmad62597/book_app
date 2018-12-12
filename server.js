7386294a75d9b13fd449ff04a7ccd32409c0ca0e
Ahmad <ahmad62596@gmail.com>
2018-12-12 13:15:14 -0800

    Server stuff

--- server.js
+++ server.js
@@ -3,18 +3,49 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const superagent = require('superagent');
+
+
 // const main = requires('/script/main.js')
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-  console.log(`listening on the coolest port: ${PORT}`);
-})
+
 app.get('/', (request, response) => {
   response.render('../views/pages/index');
 });
 
-// document.getElementById('searchBtn').addEventListener("click", function(){
-//   console.log("I am clicked");
-// }); 
+
+function getBooks (req, res) {
+  let url = 'https://www.googleapis.com/books/v1/{collectionName}/resourceID?parameters';
+    return superagent.get(url)
+  .then(data => {
+    let everyBook = data.body.items.map (data => {
+      const newBook = new Books(data);
+      return newBook;
+    });
+    return everyBook;
+
+  }).then(everyBook => res.render ('./pages/index/show', {books: everyBook}))
+    .catch(error => errorHandler(error));
+};
+
+
+function Books(data) {
+  this.title = data.volumeInfo.title || 'Not Available';
+  this.author = data.volumeInfo.author || 'Not Available';
+  this.description = data.volumeInfo.description || 'Not Available';
+  this.thumbnail= data.volumeInfo.imageLinks.thumbnail || 'https://via.placeholder.com/128x200.png';
+}
+
+
+function errorHandler (err, res) {
+  console.error (err);
+  if (res) res.status(500).send('Something is broken')
+}
+
+
+app.listen(PORT, () => {
+  console.log(`listening on the coolest port: ${PORT}`);
+})
